a702949c43e36a2b532fae6163fae246a5ccf18d
Aryan <aryan7017n@gmail.com>
2026-04-02 11:54:10 +0530

    setup routing and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+// pages import
+import Home from "./pages/Home"
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Courses from './pages/Courses'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
 
 const App = () => {
   return (
-    <div className=''>Aryan</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/courses" element={<Courses />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   )
 }
 
