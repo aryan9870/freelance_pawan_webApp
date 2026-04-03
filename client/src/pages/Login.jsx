5aa7a9058cb7ee9c2d50c5a496cb5079762f309b
Aryan <aryan7017n@gmail.com>
2026-04-03 15:03:58 +0530

    implelemt login and signup page UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,67 @@
-import React from 'react'
-import Navbar_v2 from '../components/Navbar_v2'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar_v2 from "../components/Navbar_v2";
 
 const Login = () => {
-  return (
-    <>
-    <Navbar_v2 />
-    <div className='h-screen flex justify-center items-center'>
-        <h1 className='text-2xl font-semibold'>Login page is under construction</h1>
-    </div>
-    </>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div>
+    <Navbar_v2 />
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h2 className="text-2xl font-serif mb-6">Login —</h2>
+
+        <form className="flex flex-col gap-4 w-80">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-[#49BBBD] px-3 py-2 outline-none"
+          />
+
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="border border-[#49BBBD] px-3 py-2 outline-none"
+          />
+
+          <div className="flex justify-between text-sm text-gray-600">
+            <Link to="/signup" className="cursor-pointer hover:underline">
+              New to this website?
+            </Link>
+
+            <Link to="/signup" className="cursor-pointer hover:underline">
+              Create account
+            </Link>
+          </div>
+
+          <button
+            onClick={handleSubmit}
+            className="bg-[#49BBBD] text-white py-2 mt-2 hover:bg-[#49BBBD]/80  transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,15 +1,79 @@
-import React from 'react'
-import Navbar_v2 from '../components/Navbar_v2'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar_v2 from "../components/Navbar_v2";
 
 const Signup = () => {
-  return (
-    <>
-    <Navbar_v2 />
-    <div className='h-screen flex justify-center items-center'>
-        <h1 className='text-2xl font-semibold'>Signup page is under construction</h1>
-    </div>
-    </>
-  )
-}
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default Signup
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user.name, user.email, user.password);
+  };
+
+  return (
+    <div>
+      <Navbar_v2 />
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center">
+          <h2 className="text-2xl font-serif mb-6">Sign Up —</h2>
+
+          <form className="flex flex-col gap-4 w-80">
+            <input
+              type="text"
+              name="name"
+              value={user.name}
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+              placeholder="Name"
+              className="border border-[#49BBBD] px-3 py-2 outline-none"
+            />
+
+            <input
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              placeholder="Email"
+              className="border border-[#49BBBD] px-3 py-2 outline-none"
+            />
+
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              placeholder="Password"
+              className="border border-[#49BBBD] px-3 py-2 outline-none"
+            />
+
+            <div className="flex justify-between text-sm text-gray-600">
+              <Link to="/login" className="cursor-pointer hover:underline">
+                Already have an account?
+              </Link>
+
+              <Link to="/login" className="cursor-pointer hover:underline">
+                Login
+              </Link>
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="bg-[#49BBBD] text-white py-2 mt-2 hover:bg-[#49BBBD]/80 transition"
+            >
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
